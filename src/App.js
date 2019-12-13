45db50585310df9f852f7b1417594b1d553e1294
Johnathon Hutt <johnathonhutt@gmail.com>
2019-12-13 15:27:31 -0500

    Finish functionality - add styles

--- src/App.js
+++ src/App.js
@@ -19,6 +19,22 @@ class Pom extends React.Component {
     this.initializeTimer = this.initializeTimer.bind(this);
     this.updateTime = this.updateTime.bind(this);
     this.pauseAndUnpause = this.pauseAndUnpause.bind(this);
+    this.resetState = this.resetState.bind(this);
+    this.updateCurrLength = this.updateCurrLength.bind(this);
+  }
+
+  updateCurrLength() {
+    if (!this.state.isSessionInitialized || this.state.paused) {
+      let sL;
+      if (this.state.currType === "session") {
+        sL = (this.state.sessionLength < 10) ? "0" + this.state.sessionLength + ":00" : this.state.sessionLength + ":00";
+      } else { //if currType === "break"
+        sL = (this.state.breakLength < 10) ? "0" + this.state.breakLength + ":00" : this.state.breakLength + ":00";
+      }
+      this.setState({
+        currLength: sL
+      });
+    }
   }
 
   initializeTimer() {
@@ -26,10 +42,14 @@ class Pom extends React.Component {
     console.log("Timer has been initialized");
     let intervalTime;
     if (!this.state.paused) {
-      //if session is not initialized
+      //for first play or session-break change - set time based on session/break length
       intervalTime = (this.state.currType === "session") ? this.state.sessionLength : this.state.breakLength;
     } else {
-      //if session is paused - set intervalTime to currLength in minutes
+      //if paused
+      this.setState({
+        paused: false
+      });
+      //Set intervalTime to currLength in minutes
       let time = this.state.currLength.split(":");
       intervalTime = Number(time[0]) + Number(time[1] / 60);
     }
@@ -46,16 +66,14 @@ class Pom extends React.Component {
     let currTime = new Date().getTime();
     let diff = (startTime - currTime) / 1000;
     this.setState({
-      currLength: `${Math.floor(diff/60)}:${(Math.floor(diff%60) < 10) ? "0" + Math.floor(diff%60) : Math.floor(diff%60)}`
+      currLength: `${(Math.floor(diff/60) < 10) ? "0" + Math.floor(diff/60) : Math.floor(diff/60)}:${(Math.floor(diff%60) < 10) ? "0" + Math.floor(diff%60) : Math.floor(diff%60)}`
     });
-    if (this.state.currLength <= "0:00") {
-      //make the beep happen
-      //set delay - then switch over to other time
-      setTimeout(function() {
-        this.setState({
-          currType: (this.state.currType === "break") ? "session" : "break"
-        });
-      }, 1000);
+    if (this.state.currLength === "00:00") {
+      document.getElementById("beep").play(); //play audio element
+      clearInterval(this.state.timer);
+      this.setState({
+        currType: (this.state.currType === "break") ? "session" : "break"
+      });
       this.initializeTimer();
     }
   }
@@ -64,60 +82,76 @@ class Pom extends React.Component {
     console.log("pauseAndUnpause has been activated");
     if (!this.state.paused) {
       //pause
+      clearInterval(this.state.timer);
       this.setState({
         paused: true
       });
-      clearInterval(this.state.timer);
     } else {
-      //unpause
-      this.setState({
-        paused: false
-      });
       this.initializeTimer();
     }
+  }
+
+  resetState() {
+    //used on reset click -- if reloading window doesn't pass fCC tests
+    clearInterval(this.state.timer);
+    this.setState({
+      breakLength: 5,
+      sessionLength: 25,
+      currLength: "25:00",
+      currType: "session",
+      isSessionInitialized: false,
+      paused: false,
+      timer: null
+    });
   }
 
   handleClick(event) {
     console.log(event.target.id);
     switch (event.target.id) {
       case "break-increment":
-        this.setState({
-          breakLength: (this.state.breakLength === 60) ? 60 : this.state.breakLength += 1
-        });
+        if (!this.state.isSessionInitialized || this.state.paused) {
+          this.setState({
+            breakLength: (this.state.breakLength === 60) ? 60 : this.state.breakLength += 1
+          });
+          this.updateCurrLength();
+        }
         break;
       case "break-decrement":
-        this.setState({
-          breakLength: (this.state.breakLength === 0) ? 0 : this.state.breakLength -= 1
-        });
+        if (!this.state.isSessionInitialized || this.state.paused) {
+          this.setState({
+            breakLength: (this.state.breakLength === 0) ? 0 : this.state.breakLength -= 1
+          });
+          this.updateCurrLength();
+        }
         break;
       case "session-increment":
-        this.setState({
-          sessionLength: (this.state.sessionLength === 60) ? 60 : this.state.sessionLength += 1
-        });
+        if (!this.state.isSessionInitialized || this.state.paused) {
+          this.setState({
+            sessionLength: (this.state.sessionLength === 60) ? 60 : this.state.sessionLength += 1
+          });
+          this.updateCurrLength();
+        }
         break;
       case "session-decrement":
-        this.setState({
-          sessionLength: (this.state.sessionLength === 0) ? 0 : this.state.sessionLength -= 1
-        });
-        break;
-      case "session-decrement":
-        this.setState({
-          sessionLength: this.state.breakLength += 1
-        });
+        if (!this.state.isSessionInitialized || this.state.paused) {
+          this.setState({
+            sessionLength: (this.state.sessionLength === 0) ? 0 : this.state.sessionLength -= 1
+          });
+          this.updateCurrLength();
+        }
         break;
       case "start_stop":
-        //initialize on first play
         if (!this.state.isSessionInitialized) {
           this.setState({
             isSessionInitialized: true
           });
           this.initializeTimer();
         } else {
-          //pause or unpause
           this.pauseAndUnpause();
         }
         break;
       case "reset":
+        // this.resetState();
         window.location.reload();
         break;
       default: console.log(event.target.id);
@@ -127,10 +161,21 @@ class Pom extends React.Component {
   render() {
     return (
       <div>
+        <h1>Farnsworth's <span className="line-through">Death</span> Pomodoro Clock</h1>
+        <h2>"It can do other things"</h2>
+        <div className="trapezoid"></div>
+        <div className="box">
+          <div className="clock-border">
+            <Break breakLength={this.state.breakLength} handleClick={this.handleClick} />
+            <Session sessionLength={this.state.sessionLength} handleClick={this.handleClick} />
+            <Timer currType={this.state.currType} paused={this.state.paused} isSessionInitialized={this.state.isSessionInitialized} currLength={this.state.currLength} handleClick={this.handleClick} />
+          </div>
+        </div>
         <div>
-          <Break breakLength={this.state.breakLength} handleClick={this.handleClick} />
-          <Session sessionLength={this.state.sessionLength} handleClick={this.handleClick} />
-          <Timer paused={this.state.paused} isSessionInitialized={this.state.isSessionInitialized} currLength={this.state.currLength} handleClick={this.handleClick} />
+          <div className="pad-wrapper">
+            <div className="pad"></div>
+            <div className="pad right"></div>
+          </div>
         </div>
       </div>
     );
@@ -140,7 +185,7 @@ class Pom extends React.Component {
 function Break(props) {
   return (
     <div className="unit-wrapper">
-      <p id="break-label">Break Length</p>
+      <p id="break-label" className="label">Break Length</p>
       <div id="break-length" className="display">{props.breakLength}</div>
       <div>
         <button id="break-increment" onClick={props.handleClick}>+</button>
@@ -153,7 +198,7 @@ function Break(props) {
 function Session(props) {
   return (
     <div className="unit-wrapper">
-      <p id="session-label">Session Length</p>
+      <p id="session-label" className="label">Session Length</p>
       <div id="session-length" className="display">{props.sessionLength}</div>
       <div>
         <button id="session-increment" onClick={props.handleClick}>+</button>
@@ -166,16 +211,15 @@ function Session(props) {
 function Timer(props) {
   return (
     <div className="unit-wrapper">
-      <p id="timer-label">{(props.isSessionInitialized || !props.paused) ? "Session" : "Paused"}</p>
+      <p id="timer-label" className="label">{(props.currType === "session") ? "Session" : "Break"}</p>
       <div id="time-left" className="display">{props.currLength}</div>
       <div>
-        <button id="start_stop" onClick={props.handleClick}>{(props.isSessionInitialized || !props.paused) ? "pause" : "start"}</button>
+        <button id="start_stop" className="play-button" onClick={props.handleClick}>{(!props.isSessionInitialized || props.paused) ? "start" : "pause"}</button>
         <button id="reset" onClick={props.handleClick}>reset</button>
+        <audio id="beep" src="https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3" type="audio/mp3"></audio>
       </div>
     </div>
   )
 }
 
 export default Pom;
-
-//find out how to use just one value - isSessionInitialized or paused
